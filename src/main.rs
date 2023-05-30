ccfa56584c98dfe0ecde5cf69c350aa98076aa90
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-05-30 18:24:33 +0800

    change: finished

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,55 @@
-use cairo;
+use std::f64::consts::PI;
+
+use cairo::{self, Context};
 use pango;
 
-const WIDTH: f64 = 256.0;
-const HEIGHT: f64 = 256.0;
+const RADIUS: f64 = 150_f64;
+const N_WORDS: i32 = 10;
 
-fn main() {
-    let surface = cairo::ImageSurface::create(cairo::Format::ARgb32, 100, 30).unwrap();
-    let cr = cairo::Context::new(&surface).unwrap();
-    cr.rectangle(0_f64, 0_f64, 100_f64, 30_f64);
-    cr.set_source_rgb(0_f64, 0_f64, 0.5);
-    cr.fill().unwrap();
-
-    let pango_scale = pango::units_from_double(1_f64);
-
-    let layout = pangocairo::create_layout(&cr);
+fn draw_text(content: &Context) {
+    let pango_scale = pango::SCALE;
+    println!("{pango_scale}");
+    content.translate(RADIUS, RADIUS);
+    let pangolayout = pangocairo::create_layout(content);
     let mut desc = pango::FontDescription::new();
+    pangolayout.set_text("Text");
+
     desc.set_family("Sans");
     desc.set_weight(pango::Weight::Bold);
-    desc.set_size(48 * pango_scale);
-    layout.set_font_description(Some(&desc));
+    desc.set_size(27 * pango_scale);
+    pangolayout.set_font_description(Some(&desc));
 
-    layout.set_markup("A <b>bold</b>\nidea");
-    layout.set_alignment(pango::Alignment::Center);
+    for i in 0..N_WORDS {
+        let angle: f64 = (360 * i) as f64 / N_WORDS as f64;
 
-    let (_ink_box, log_box) = layout.extents();
-    let (text_width, text_height): (f64, f64) = (
-        1.0 * (log_box.width() as f64) / (pango_scale as f64),
-        1.0 * (log_box.height() as f64) / (pango_scale as f64),
-    );
+        content.save().unwrap();
+        let red: f64 = (1_f64 + f64::cos((angle - 60.0) * PI / 180_f64)) / 2_f64;
 
-    cr.move_to(
-        WIDTH / 2.0 - text_width / 2.0,
-        HEIGHT / 2.0 - text_height / 2.0,
-    );
-    cr.set_source_rgb(1.0, 1.0, 1.0);
-    pangocairo::show_layout(&cr, &layout);
-    cr.fill().unwrap();
+        content.set_source_rgb(red, 0.0, 1.0 - red);
+        content.rotate(angle * PI / 180.0);
+
+        pangocairo::update_layout(content, &pangolayout);
+
+        let (width, _height) = pangolayout.size();
+
+        content.move_to(
+            (width as f64 / pango_scale as f64) * -1.0 / 2.0,
+            -1.0 * RADIUS,
+        );
+        pangocairo::show_layout(content, &pangolayout);
+        content.restore().unwrap();
+    }
+}
+
+fn main() {
+    let surface =
+        cairo::ImageSurface::create(cairo::Format::ARgb32, 2 * RADIUS as i32, 2 * RADIUS as i32)
+            .unwrap();
+    let cr = cairo::Context::new(&surface).unwrap();
+    cr.set_source_rgb(1_f64, 1_f64, 1_f64);
+    cr.paint().unwrap();
+
+    draw_text(&cr);
 
     let mut writer = std::fs::File::create("test.png").unwrap();
     surface.write_to_png(&mut writer).unwrap();
